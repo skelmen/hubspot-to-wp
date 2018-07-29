cd64794f92d698b2f9033d442d1070639041601a
skelmen <zatin13@gmail.com>
2018-07-29 15:22:04 +0300

    fix2

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-var express = require('express');
-var app = express();
 var fs = require("fs");
 var path = require('path');
 var request = require('request');
@@ -12,9 +10,7 @@ for (i in jsonContent.objects) {
   var url = jsonContent.objects[i].url;
   var filename = path.basename(jsonContent.objects[i].url);
   var ext = path.extname(filename);
-  // request(url).pipe(fs.createWriteStream('files/' + filename));
   if(ext == '.png' || ext == '.jpg' || ext == '.jpeg' || ext == '.pdf'){
     request(url).pipe(fs.createWriteStream('files/' + filename));
   }
-  // console.log(path.extname(filename));
 }
